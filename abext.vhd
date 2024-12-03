0323aa4f017e6c3d00c1924a534c8576aa56dd2d
Ênio <146883699+Enio-Antonio@users.noreply.github.com>
2024-12-03 11:47:55 -0300

    Update abext.vhd

--- abext.vhd
+++ abext.vhd
@@ -4,7 +4,7 @@ end abext;
 
 architecture main of abext is
 begin
-	ia <= (not x and not y and not z and a and not b) or (not x and not y and not z and a and b) or (not x and not y and z and a and not b) or 
+	ia <= (not x and not y and not z and a and not b) or (not x and not y and not z and a and b) or (not x and not y and z and a and not b) or (not x and not y and z and a and b) or 
 	(not x and not y and not z and a and b) or (not x and y and not z and a and not b) or (not x and y and not z and a and b) or 
 	(not x and y and z and a and not b) or (not x and y and z and a and b) or (x and not y and not z and a and b) or 
 	(x and not y and z and a and not b) or (x and not y and z and not a and b) or (x and not y and z and a and b) or 
