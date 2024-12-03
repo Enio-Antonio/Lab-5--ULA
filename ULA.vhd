29be64038a85c6877a66e3afa6679b5950a6ef17
Enio-Antonio <enioaclizieri@gmail.com>
2024-12-03 20:39:12 -0300

    Full adder de 6 bits

--- ULA.vhd
+++ ULA.vhd
@@ -1,5 +1,5 @@
 entity ULA is
-	port(A, B : in bit_vector(5 downto 0); x, y, z, clk, enable : in bit; saida : out bit_vector(5 downto 0); cin_teste : out bit);
+	port(A, B : in bit_vector(5 downto 0); x, y, z, clk, enable : in bit; saida : out bit_vector(5 downto 0));
 end ULA;
 
 architecture main of ULA is
@@ -15,29 +15,18 @@ architecture main of ULA is
 		port(x, y, z : in bit; cin : out bit);
 	end component;
 	
-	component calculadora is
-		port(a, b : in bit_vector (5 downto 0); sub : in bit; result : out bit_vector (5 downto 0));
-	end component;
-	
 	component full_adder is 
-		port(a, b, ci : in bit; s, co : out bit);
+		port(a, b : in bit_vector(5 downto 0); s : out bit_vector; ci : in bit);
 	end component;
 	
 	component reg6 is
 		 Port (
-			  clk : in bit;                       -- Sinal de clock
+			  clk : in bit;                       
 			  enable : in bit;
-			  data_in : in bit_vector(5 downto 0); -- Entrada de 6 bits
-			  data_out : out bit_vector(5 downto 0) -- Saída de 6 bits
+			  data_in : in bit_vector(5 downto 0); 
+			  data_out : out bit_vector(5 downto 0)
 		 );
 	end component;
-
-	signal co_0 : bit;
-	signal co_1 : bit;
-	signal co_2 : bit;
-	signal co_3 : bit;
-	signal co_4 : bit;
-	signal co_5 : bit;
 
 begin
 	abext0: abext port map(x => x, y => y, z => z, a => A(0), b => B(0), ia => IA(0), ib => IB(0));
@@ -48,15 +37,8 @@ begin
 	abext5: abext port map(x => x, y => y, z => z, a => A(5), b => B(5), ia => IA(5), ib => IB(5));
 	
 	cinext_0: cinext port map(x => x, y => y, z => z, cin => cin);
-	cinext_teste : cinext port map(x => x, y => y, z => z, cin => cin_teste);
 	
-	-- somador: calculadora port map(a => IA, b => IB, sub => cin, result => saida);
-	somador0: full_adder port map(a => IA(0), b => IB(0), ci => cin, co => co_0, s => saida_ula_interna(0));
-	somador1: full_adder port map(a => IA(1), b => IB(1), ci => co_0, co => co_1, s => saida_ula_interna(1));
-	somador2: full_adder port map(a => IA(2), b => IB(2), ci => co_1, co => co_2, s => saida_ula_interna(2));
-	somador3: full_adder port map(a => IA(3), b => IB(3), ci => co_2, co => co_3, s => saida_ula_interna(3));
-	somador4: full_adder port map(a => IA(4), b => IB(4), ci => co_3, co => co_4, s => saida_ula_interna(4));
-	somador5: full_adder port map(a => IA(5), b => IB(5), ci => co_4, co => co_5, s => saida_ula_interna(5));
+	somador: full_adder port map(a => IA, b => IB, ci => cin, s => saida_ula_interna);
 
 	registrador : reg6
 	port map(clk => clk, enable => enable, data_in => saida_ula_interna , data_out => saida);
--- full_adder.vhd
+++ full_adder.vhd
@@ -1,9 +1,30 @@
 entity full_adder is 
-	port(a, b, ci : in bit; co, s : out bit);
+	port(a, b : in bit_vector(5 downto 0); s : out bit_vector; ci : in bit);
 end full_adder;
 
 architecture main of full_adder is
+	signal co : bit_vector(5 downto 0);
+	
 begin
-	co <= (not(a) and b and ci) or (a and not(b) and ci) or (a and b and not(ci)) or (a and b and ci);
-	s <= (not(a) and not(b) and ci) or (not(a) and b and not(ci)) 	or (a and not(b) and not(ci)) or (a and b and ci);
+	co(0) <= (not(a(0)) and b(0) and ci) or (a(0) and not(b(0)) and ci) or (a(0) and b(0) and not(ci)) or (a(0) and b(0) and ci);
+	s(0) <= (not(a(0)) and not(b(0)) and ci) or (not(a(0)) and b(0) and not(ci)) or (a(0) and not(b(0)) and not(ci)) or (a(0) and b(0) and ci);
+	
+	co(1) <= (not(a(1)) and b(1) and co(0)) or (a(1) and not(b(1)) and co(0)) or (a(1) and b(1) and not(co(0))) or (a(1) and b(1) and co(0));
+	s(1) <= (not(a(1)) and not(b(1)) and co(0)) or (not(a(1)) and b(1) and not(co(0))) or (a(1) and not(b(1)) and not(co(0))) or (a(1) and b(1) and co(0));
+	
+	co(2) <= (not(a(2)) and b(2) and co(1)) or (a(2) and not(b(2)) and co(1)) or (a(2) and b(2) and not(co(1))) or (a(2) and b(2) and co(1));
+	s(2) <= (not(a(2)) and not(b(2)) and co(1)) or (not(a(2)) and b(2) and not(co(1))) or (a(2) and not(b(2)) and not(co(1))) or (a(2) and b(2) and co(1));
+
+	
+	co(3) <= (not(a(3)) and b(3) and co(2)) or (a(3) and not(b(3)) and co(2)) or (a(3) and b(3) and not(co(2))) or (a(3) and b(3) and co(2));
+	s(3) <= (not(a(3)) and not(b(3)) and co(2)) or (not(a(3)) and b(3) and not(co(2))) or (a(3) and not(b(3)) and not(co(2))) or (a(3) and b(3) and co(2));
+
+	
+	co(4) <= (not(a(4)) and b(4) and co(3)) or (a(4) and not(b(4)) and co(3)) or (a(4) and b(4) and not(co(3))) or (a(4) and b(4) and co(3));
+	s(4) <= (not(a(4)) and not(b(4)) and co(3)) or (not(a(4)) and b(4) and not(co(3))) or (a(4) and not(b(4)) and not(co(3))) or (a(4) and b(4) and co(3));
+
+	
+	co(5) <= (not(a(5)) and b(5) and co(4)) or (a(5) and not(b(5)) and co(4)) or (a(5) and b(5) and not(co(4))) or (a(5) and b(5) and co(4));
+	s(5) <= (not(a(5)) and not(b(5)) and co(4)) or (not(a(5)) and b(5) and not(co(4))) or (a(5) and not(b(5)) and not(co(4))) or (a(5) and b(5) and co(4));
+	
 end architecture main;	
